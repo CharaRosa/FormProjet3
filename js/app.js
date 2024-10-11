02e8437c696e0821f2de0906f3c07921bf63574a
CharaRosa <loelondjerose@gmail.com>
2024-10-11 10:07:48 +0100

    setfilter commit

---
+++ js/app.js
@@ -0,0 +1,54 @@
+// code pour  afficher les images dynamiquement
+async function getWorks() {
+    const url = "http://localhost:5678/api/works";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        console.log(json);
+        for(let i = 0; i< json.length; i++){
+          setFigure(json[i])
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+    getWorks();
+// fonction pour  afficher les images dynamiquement
+    function setFigure(data){
+      const figure =document.createElement("figure");
+      figure.innerHTML=`<img src=${data.imageUrl} alt=${data.title}>
+      <figcaption>${data.title}</figcaption>`;
+      document.querySelector(".gallery").append(figure);
+    }
+    
+
+    // code pour  afficher les images dynamiquement
+async function getCategories() {
+  const url = "http://localhost:5678/api/categories";
+  try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.json();
+      console.log(json);
+      for(let i = 0; i< json.length; i++){
+        setFilter(json[i])
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  getCategories();
+
+  // fonction pour pour les filtres
+  
+  function setFilter(data){
+    const div = document.createElement("div");
+    div.innerHTML=`${data.name}`;
+    document.querySelector(".div-container").append(div);
+  }
+  
